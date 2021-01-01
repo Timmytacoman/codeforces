1d248e51259f9e387b4532633650d7b67fe5036e
Timmytacoman <72770854+Timmytacoman@users.noreply.github.com>
2021-01-01 14:14:28 -0700

    first two of problemset

---
+++ way_too_long_words.py
@@ -0,0 +1,7 @@
+num_words = int(input())
+for i in range(num_words):
+    word = input()
+    if len(word) <= 10:
+        print(word)
+    else:
+        print(word[0] + str(len(word) - 2) + word[-1])
