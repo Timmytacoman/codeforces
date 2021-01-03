cea5e0268602000f0ffa0ef5009f80ae70dca49d
Timmytacoman <72770854+Timmytacoman@users.noreply.github.com>
2021-01-02 18:13:26 -0700

    1100

---
+++ 1100/taxi.py
@@ -0,0 +1,4 @@
+import math
+input()
+nums = list(map(int, input().split()))
+print(math.ceil(sum(nums) / 4))
---
+++ 900/football.py
@@ -0,0 +1,5 @@
+num = input()
+if num.find("0000000") != -1 or num.find("1111111") != -1:
+    print("YES")
+else:
+    print("NO")
